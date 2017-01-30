22582bc0d31a8d82390cc33d9e2ff77bfa874087
spatiumlucis <spatiumlucis@gmail.com>
2017-01-29 22:39:58 -0600

    Commit 1-29-2017
    Added code for lighting_controller.py

--- lighting_subsystem/lighting_sub.py
+++ lighting_subsystem/lighting_sub.py
@@ -12,16 +12,36 @@ import wiringpi
 lightIntensity = -5
 
 #*Set up PWM for lights
-pinPWM = 18
-pinRelay = 7
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(pinRelay, GPIO.OUT)
-
+# pinPWM = 18
+# pinRelay = 7
+#
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(pinRelay, GPIO.OUT)
+#
+# wiringpi.wiringPiSetupGpio()
+# wiringpi.pinMode(pinPWM, 2)
+# wiringpi.pwmSetClock(5000)
+pinRelay = 4
 
 wiringpi.wiringPiSetupGpio()
-wiringpi.pinMode(pinPWM, 2)
-wiringpi.pwmSetClock(5000)
+GPIO.setmode(GPIO.BCM) #choose BCM or BOARD numbering schemes
+GPIO.setup(17, GPIO.OUT) #set GIPO 17 as red led
+GPIO.setup(27, GPIO.OUT) #set GIPO 27 as green led
+GPIO.setup(22, GPIO.OUT) #set GIPO 22 as blue led
+GPIO.setup(pinRelay, GPIO.OUT)
+
+red = GPIO.PWM(17, 100) #create object red for PWM on port 17 at 100Hz
+green = GPIO.PWM(27, 100) #create object red for PWM on port 27 at 100Hz
+blue = GPIO.PWM(22, 100) #create object red for PWM on port 22 at 100Hz
+
+red.start(0)
+green.start(0)
+blue.start(0)
+
+GPIO.output(pinRelay, GPIO.HIGH)
+
+pause_time = 0.02
+
 
 # Open database connection
 db = MySQLdb.connect(host="192.168.1.6", port=3306, user="spatiumlucis", passwd="spatiumlucis", db="ilcs")
@@ -68,13 +88,14 @@ def begin_threading():
     try:
         thread.start_new_thread(PIR_cmd, ())
         #thread.start_new_thread(light_cmd, ())
-
-        # Add RGB Ballish her
     except:
         print "Error: unable to start thread"
     light_cmd()
 
 def PIR_cmd():
+    global red
+    global green
+    global blue
     # *establish server socket for Control subsystem to connect to for LI send
     lighting_pir_svr_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)  # * Create a socket object
     lighting_pir_svr_sock_host = ''  # * Get local machine name
@@ -93,11 +114,21 @@ def PIR_cmd():
         light_intensity = lighting_pir_svr_sock_connection.recv(1024)
         if int(light_intensity) == 0:
             # * turn lights off
-            GPIO.output(pinRelay, GPIO.LOW)
-        else:
             GPIO.output(pinRelay, GPIO.HIGH)
-            dutyCycle = (float(light_intensity) / 100) * 1024
-            wiringpi.pwmWrite(pinPWM, int(dutyCycle))
+            red.stop()
+            green.stop()
+            blue.stop()
+            GPIO.cleanup()
+            #GPIO.output(pinRelay, GPIO.LOW)
+
+        else:
+            #sepatate into red, green, blue values
+            red.ChangeDutyCycle()
+            green.ChangeDutyCycle()
+            blue.ChangeDutyCycle()
+            #GPIO.output(pinRelay, GPIO.HIGH)
+            #dutyCycle = (float(light_intensity) / 100) * 1024
+            #wiringpi.pwmWrite(pinPWM, int(dutyCycle))
         time.sleep(1)
 
         lighting_pir_svr_sock_connection.close()
