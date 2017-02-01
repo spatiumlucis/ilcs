dd5d18da492cecef6819710470b4432bad8b102e
zps0011 <zacharysimpson@my.unt.edu>
2017-02-01 11:21:15 -0600

    2-1-17
    added backups

--- sensor_subsystem/bkup/sensor_sub_1-31-17.py
+++ sensor_subsystem/bkup/sensor_sub_1-31-17.py
@@ -6,7 +6,6 @@ import socket
 from threading import Lock
 import RPi.GPIO as GPIO  # Import GPIO library
 import MySQLdb  # Required for MySQL stuff
-import smbus
 
 # Open database connection
 print "Establishing Database connection.....\n"
@@ -41,7 +40,7 @@ time_mutex = Lock()
 
 save_mutex = Lock()
 
-local_ip = ""
+
 def get_ip():
     # source:
     # http://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib/25850698#25850698
@@ -66,7 +65,7 @@ def boot_up():
     global LIGHT_THRESHOLD
     global WAKE_UP_TIME
     global SLEEP_MODE_STATUS
-    global local_ip
+
     init_circadian_table()
     print "Circadian table created..."
     local_ip = get_ip()
@@ -399,96 +398,6 @@ def RGB_sensor():
     global COLOR_THRESHOLD
     global LIGHT_THRESHOLD
     global WAKE_UP_TIME
-    global local_ip
-
-    bus = smbus.SMBus(1)
-    # I2C address 0x29
-    # Register address must be OR'ed wit 0x80
-    bus.write_byte(0x29, 0x80 | 0x12)
-    ver = bus.read_byte(0x29)
-    # version # should be 0x44
-
-    hardRed = 11850
-    hardGreen = 24108
-    hardBlue = 22985
-    hardClear = 60280
-    hardLux = 17378
-
-    if ver == 0x44:
-        print "Device found\n"
-        bus.write_byte(0x29, 0x80 | 0x00)  # 0x00 = ENABLE register
-        bus.write_byte(0x29, 0x01 | 0x02)  # 0x01 = Power on, 0x02 RGB sensors enabled
-        bus.write_byte(0x29, 0x80 | 0x14)  # Reading results start register 14, LSB then MSB
-        while True:
-            if SLEEP_MODE_STATUS == 0:
-                data = bus.read_i2c_block_data(0x29, 0)
-                clear = clear = data[1] << 8 | data[0]
-                red = data[3] << 8 | data[2]
-                green = data[5] << 8 | data[4]
-                blue = data[7] << 8 | data[6]
-                lux = int((-0.32466 * red) + (1.57837 * green) + (-0.73191 * blue))
-                #crgbl = "C: %s, R: %s, G: %s, B: %s, Lux: %s" % (clear, red, green, blue, lux)
-
-                sql = """UPDATE sensor_status SET red = %s WHERE ip = %s"""
-                try:
-                    # Execute the SQL command
-                    cursor.execute(sql, (red, local_ip))
-                    # Commit your changes in the database
-                    db.commit()
-                except:
-                    # Rollback in case there is any error
-                    db.rollback()
-                sql = """UPDATE sensor_status SET green = %s WHERE ip = %s"""
-                try:
-                    # Execute the SQL command
-                    cursor.execute(sql, (green, local_ip))
-                    # Commit your changes in the database
-                    db.commit()
-                except:
-                    # Rollback in case there is any error
-                    db.rollback()
-                sql = """UPDATE sensor_status SET blue = %s WHERE ip = %s"""
-                try:
-                    # Execute the SQL command
-                    cursor.execute(sql, (blue, local_ip))
-                    # Commit your changes in the database
-                    db.commit()
-                except:
-                    # Rollback in case there is any error
-                    db.rollback()
-
-                # print crgbl
-                # if red >= 10 and red < (hardRed * .95):
-                #     redFlag = True
-                #     print "Red Degradation"
-                # else:
-                #     redFlag = False
-                # if green >= 10 and green < (hardGreen * .95):
-                #     greenFlag = True
-                #     print "Green Degradation"
-                # else:
-                #     greenFlag = False
-                # if blue >= 10 and blue < (hardBlue * .95):
-                #     blueFlag = True
-                #     print "Blue Degradation"
-                # else:
-                #     blueFlag = False
-                # if clear >= 10 and clear < (hardClear * .95):
-                #     clearFlag = True
-                #     print "Clear Degradation"
-                # else:
-                #     clearFlag = False
-                # if lux >= 10 and lux < (hardLux * .95):
-                #     luxFlag = True
-                #     print "Intensity Degradation\n"
-                # if lux < 10:
-                #     luxFlag = False
-                #     print "Lights OFF\n"
-                # else:
-                #     luxFlag = False
-                time.sleep(2)
-    else:
-        print "Device not found\n"
 
 
 def USR_sensor():
